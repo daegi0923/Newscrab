00cce1c9cacb33149341460f74d561ef79555a54
김동건 <sky4tony@naver.com>
2024-09-04 16:47:26 +0900

    스프링 프로젝트 생성

---
+++ newscrab/src/main/java/com/gihojise/newscrab/NewscrabApplication.java
@@ -0,0 +1,13 @@
+package com.gihojise.newscrab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewscrabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewscrabApplication.class, args);
+	}
+
+}
