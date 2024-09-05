89ff520a89418c658ed1a1ce840d07a248f1eaa7
김동건 <sky4tony@naver.com>
2024-09-05 17:49:38 +0900

    [Init] #2 유저, 잔디 엔티티생성

---
+++ newscrab/src/main/java/com/gihojise/newscrab/entity/Grass.java
@@ -0,0 +1,31 @@
+package com.gihojise.newscrab.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "grass") // 테이블명 지정
+@Getter
+public class Grass {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long grassId; // 잔디 ID (Primary Key)
+
+    @ManyToOne(fetch = FetchType.LAZY) // 다대일 관계, 여러 잔디는 하나의 사용자와 연결
+    @JoinColumn(name = "user_id", nullable = false) // FK로 user_id를 참조
+    private User user; // 사용자 ID (Foreign Key)
+
+    @Column(nullable = false)
+    private LocalDate createdAt; // 날짜
+
+    @Column(nullable = false)
+    private int count; // 개수
+
+    // 카운트를 증가시키는 비즈니스 로직
+    public void increaseCount() {
+        this.count += 1; // 카운트 1 증가
+    }
+
+}
