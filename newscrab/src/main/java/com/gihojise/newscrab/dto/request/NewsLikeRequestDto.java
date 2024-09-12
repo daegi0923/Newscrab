36652a0c835554dec3d4581f23f3c706bec38e03
차봉석 <chaqhdtjr7@gmail.com>
2024-09-12 11:12:34 +0900

    [Remove] #8 NewsLikeRequestDto 삭제 찜 하기 기능에서 newsId는 PathVariable 로 받기에 requestDto는 불필요

---
+++ newscrab/src/main/java/com/gihojise/newscrab/dto/request/NewsLikeRequestDto.java
@@ -0,0 +1,10 @@
+package com.gihojise.newscrab.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class NewsLikeRequestDto {
+    private int newsId;
+}
