1800c1ed7e1fa1af2836660f441781c819426948
박범준 <fizd7@naver.com>
2024-09-13 11:41:08 +0900

    [Fix] #4 : 파일명 대소문자 수정

--- FE-newscrab/newscrab/src/components/common/Header.tsx
+++ FE-newscrab/newscrab/src/components/common/Header.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import headerImage from "@assets/header.png";
+import headerImage from "@assets/headerImage.png";
 
 const HeaderContainer = styled.div`
   display: flex;
