da03e7fdf93e929ef277b9d67f04e16887c35bd1
문자영 <mremi99@naver.com>
2024-09-14 15:16:01 +0900

    [Feat] #7 : 단어장 페이지네이션 지정

--- FE-newscrab/newscrab/src/pages/voca/VocaPage.tsx
+++ FE-newscrab/newscrab/src/pages/voca/VocaPage.tsx
@@ -8,6 +8,7 @@ import { words } from "@components/voca/VocaList";
 import AdImage from "@components/common/Advertise";
 import Card from "@components/voca/VocaCard";
 import DropDown from "@components/common/DropDown"
+import Pagination from "@components/voca/Pagination"
 
 interface MockWord {
   vocaId: number;
@@ -42,7 +43,16 @@ const mockWords = [
   { vocaId: 7, industryId: 8, vocaName: "전기차", vocaDesc: "전기차입니다.", originNewsId: 107, sentence: "전기차 시장이 빠르게 성장하고 있습니다.", createdAt: "2024-11-07T12:00:00", updatedAt: "2024-11-07T12:00:00", related_news_id1: 219, related_news_id2: 220, related_news_id3: 221 }, 
   { vocaId: 6, industryId: 11, vocaName: "컴퓨터 칩", vocaDesc: "컴퓨터 칩입니다.", originNewsId: 106, sentence: "반도체 산업의 수요가 급증하고 있습니다.", createdAt: "2024-10-07T12:00:00", updatedAt: "2024-10-07T12:00:00", related_news_id1: 216, related_news_id2: 217, related_news_id3: 218 },
   { vocaId: 4, industryId: 13, vocaName: "하리보", vocaDesc: "하리보입니다.", originNewsId: 104, sentence: "정유 업계의 수익이 증가하고 있습니다.", createdAt: "2024-08-07T12:00:00", updatedAt: "2024-08-07T12:00:00", related_news_id1: 210, related_news_id2: 211, related_news_id3: 212 },
-  { vocaId: 8, industryId: 2, vocaName: "인공지능", vocaDesc: "인공지능입니다.", originNewsId: 108, sentence: "AI 기술이 다양한 산업에 도입되고 있습니다.", createdAt: "2024-12-07T12:00:00", updatedAt: "2024-12-07T12:00:00", related_news_id1: 222, related_news_id2: 223, related_news_id3: 224 } 
+  { vocaId: 8, industryId: 2, vocaName: "인공지능", vocaDesc: "인공지능입니다.", originNewsId: 108, sentence: "AI 기술이 다양한 산업에 도입되고 있습니다.", createdAt: "2024-12-07T12:00:00", updatedAt: "2024-12-07T12:00:00", related_news_id1: 222, related_news_id2: 223, related_news_id3: 224 },
+  { vocaId: 9, industryId: 4, vocaName: "치코리타", vocaDesc: "치코리타입니다.", originNewsId: 102, sentence: "바이오헬스 산업이 급성장 중입니다.", createdAt: "2024-06-07T12:00:00", updatedAt: "2024-06-07T12:00:00", related_news_id1: 204, related_news_id2: 205, related_news_id3: 206 }, 
+  { vocaId: 10, industryId: 2, vocaName: "LNG선", vocaDesc: "LNG선입니다.", originNewsId: 101, sentence: "조선업이 호황을 맞고 있습니다.", createdAt: "2024-05-07T12:00:00", updatedAt: "2024-05-07T12:00:00", related_news_id1: 201, related_news_id2: 202, related_news_id3: 203 }, 
+  { vocaId: 11, industryId: 9, vocaName: "오트밀", vocaDesc: "오트밀입니다.", originNewsId: 103, sentence: "금융 시장의 변동성이 큽니다.", createdAt: "2024-07-07T12:00:00", updatedAt: "2024-07-07T12:00:00", related_news_id1: 207, related_news_id2: 208, related_news_id3: 209 },
+  { vocaId: 12, industryId: 4, vocaName: "꼬숨이", vocaDesc: "꼬숨이입니다.", originNewsId: 105, sentence: "철강 산업의 생산량이 증가하고 있습니다.", createdAt: "2024-09-07T12:00:00", updatedAt: "2024-09-07T12:00:00", related_news_id1: 213, related_news_id2: 214, related_news_id3: 215 }, 
+  { vocaId: 13, industryId: 8, vocaName: "전기차", vocaDesc: "전기차입니다.", originNewsId: 107, sentence: "전기차 시장이 빠르게 성장하고 있습니다.", createdAt: "2024-11-07T12:00:00", updatedAt: "2024-11-07T12:00:00", related_news_id1: 219, related_news_id2: 220, related_news_id3: 221 }, 
+  { vocaId: 14, industryId: 11, vocaName: "컴퓨터 칩", vocaDesc: "컴퓨터 칩입니다.", originNewsId: 106, sentence: "반도체 산업의 수요가 급증하고 있습니다.", createdAt: "2024-10-07T12:00:00", updatedAt: "2024-10-07T12:00:00", related_news_id1: 216, related_news_id2: 217, related_news_id3: 218 },
+  { vocaId: 15, industryId: 13, vocaName: "하리보", vocaDesc: "하리보입니다.", originNewsId: 104, sentence: "정유 업계의 수익이 증가하고 있습니다.", createdAt: "2024-08-07T12:00:00", updatedAt: "2024-08-07T12:00:00", related_news_id1: 210, related_news_id2: 211, related_news_id3: 212 },
+  { vocaId: 16, industryId: 2, vocaName: "인공지능", vocaDesc: "인공지능입니다.", originNewsId: 108, sentence: "AI 기술이 다양한 산업에 도입되고 있습니다.", createdAt: "2024-12-07T12:00:00", updatedAt: "2024-12-07T12:00:00", related_news_id1: 222, related_news_id2: 223, related_news_id3: 224 },
+
 ]; 
 
 const SearchContainer = styled.div``;
@@ -82,6 +92,11 @@ const VocaPage: React.FC = () => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
   const [selectedIndustryId, setSelectedIndustryId] = useState<number | null>(null);
   const [sortedWords, setSortedWords] = useState<MockWordWithImages[]>(mappedWords);
+  
+  // 페이지네이션
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const itemPerPage = 10; // 한 페이지에 열 개
+  const totalPages = Math.ceil(sortedWords.length / itemPerPage);
 
   const sortWords = (filterType: string, wordsToSort: MockWordWithImages[]): MockWordWithImages[] => {
     let sorted;
@@ -97,6 +112,13 @@ const VocaPage: React.FC = () => {
 
   const filterIndustry = (industryId: number | null) => {
     return industryId ? mappedWords.filter(word => word.industryId === industryId) : mappedWords;
+  };
+
+  // 현재 페이지에 맞는 데이터 추출
+  const getCurrentPageData = () => {
+    const startIndex = (currentPage - 1) * itemPerPage;
+    const endIndex = startIndex + itemPerPage;
+    return sortedWords.slice(startIndex, endIndex);
   };
 
   useEffect(() => {
@@ -118,6 +140,18 @@ const VocaPage: React.FC = () => {
     setDropdownOpen(false);
   };
 
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   return (
     <div>
       <GlobalStyle />
@@ -130,7 +164,7 @@ const VocaPage: React.FC = () => {
               <DropDown words={words} handleIndustrySelect={handleIndustrySelect} />
             )}
             <CardContainer>
-              {sortedWords.map((word) => (
+              {getCurrentPageData().map((word) => (
                 <Card 
                   key={word.vocaId}
                   img={word.img}
@@ -143,6 +177,7 @@ const VocaPage: React.FC = () => {
           </VocaContainer>
           <AdImage />
         </AdContainer>
+        {totalPages > 1 &&(<Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} onPrevPage={handlePrevPage} onNextPage={handleNextPage}/>)}
       </SearchContainer>
     </div>
   );
