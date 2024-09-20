321de838f87ba714fd39af646c2ddc492b6e119d
강수연 <kangtea9@naver.com>
2024-09-20 16:06:37 +0900

    [Feat] #11 : 프로젝트 DB 맞춤 API 수정

--- data/app/crawling/schemas.py
+++ data/app/crawling/schemas.py
@@ -1,27 +1,35 @@
 from pydantic import BaseModel
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 
 class NewsBase(BaseModel):
     news_url: str
     news_title: str
     news_content: str
-    news_company: str
-    news_published_at: datetime
-    view: Optional[int] = 0
-    scrapCnt: Optional[int] = 0
-    related_news_id_1: Optional[int] = None
-    related_news_id_2: Optional[int] = None
-    related_news_id_3: Optional[int] = None
+    news_company: Optional[str]
+    news_published_at: Optional[datetime]
 
 class NewsCreate(NewsBase):
-    news_industry_id: int
+    news_industry_id: int  
+
+class NewsUpdate(BaseModel):
+    news_url: Optional[str]
+    news_title: Optional[str]
+    news_content: Optional[str]
+    news_company: Optional[str]
+    news_published_at: Optional[datetime]
+    news_industry_id: Optional[int]  
 
 class News(NewsBase):
     news_id: int
-    news_industry_id: int
+    news_industry_id: int  
     created_at: datetime
     updated_at: datetime
+    view: int
+    scrapCnt: int
+    related_news_id_1: Optional[int]
+    related_news_id_2: Optional[int]
+    related_news_id_3: Optional[int]
 
     class Config:
         orm_mode = True
