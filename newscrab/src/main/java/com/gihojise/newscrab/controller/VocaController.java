b1db71a3deaedbcd532633d49195ce51e6963cb8
차봉석 <chaqhdtjr7@gmail.com>
2024-09-22 00:17:54 +0900

    [Docs] #13 Swagger API 문서작업

--- newscrab/src/main/java/com/gihojise/newscrab/controller/VocaController.java
+++ newscrab/src/main/java/com/gihojise/newscrab/controller/VocaController.java
@@ -5,6 +5,8 @@ import com.gihojise.newscrab.dto.request.VocaAddRequestDto;
 import com.gihojise.newscrab.dto.response.VocaListResponseDto;
 import com.gihojise.newscrab.dto.response.VocaResponseDto;
 import com.gihojise.newscrab.service.VocaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +15,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/voca")
 @RequiredArgsConstructor
+@Tag(name = "Voca Controller", description = "단어 관리 API") // 컨트롤러 설명 추가
 public class VocaController {
 
     private final VocaService vocaService;
 
     // 1. 단어 목록 조회
+    @Operation(summary = "단어 목록 조회", description = "등록된 모든 단어 목록을 조회합니다.")
     @GetMapping
     public ResponseEntity<ApiResponse<VocaListResponseDto>> getVocaList() {
         VocaListResponseDto response = vocaService.getVocaList();
@@ -25,6 +29,7 @@ public class VocaController {
     }
 
     // 2. 단어 상세 조회
+    @Operation(summary = "단어 상세 조회", description = "단어 ID로 단어 상세 정보를 조회합니다.")
     @GetMapping("/{termId}")
     public ResponseEntity<ApiResponse<VocaResponseDto>> getVocaDetail(@PathVariable int termId) {
         VocaResponseDto response = vocaService.getVocaDetail(termId);
@@ -32,6 +37,7 @@ public class VocaController {
     }
 
     // 3. 단어 추가
+    @Operation(summary = "단어 추가", description = "단어를 추가합니다.")
     @PostMapping
     public ResponseEntity<ApiResponse<Void>> addVoca(@RequestParam int userId, @RequestBody VocaAddRequestDto vocaAddRequestDto) {
         vocaService.addVoca(vocaAddRequestDto, userId);
@@ -39,6 +45,7 @@ public class VocaController {
     }
 
     // 4. 단어 수정
+    @Operation(summary = "단어 수정", description = "단어를 수정합니다.")
     @PutMapping("/{termId}")
     public ResponseEntity<ApiResponse<Void>> updateVoca(@RequestParam int userId, @PathVariable int termId, @RequestBody VocaAddRequestDto vocaAddRequestDto) {
         vocaService.updateVoca(userId, termId, vocaAddRequestDto);
@@ -46,6 +53,7 @@ public class VocaController {
     }
 
     // 5. 단어 삭제
+    @Operation(summary = "단어 삭제", description = "단어를 삭제합니다.")
     @DeleteMapping("/{termId}")
     public ResponseEntity<ApiResponse<Void>> deleteVoca(@RequestParam int userId, @PathVariable int termId) {
         vocaService.deleteVoca(userId, termId);
