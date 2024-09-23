d88c14e61a0804b79da09c0a8645fc6046f6f3bc
강수연 <kangtea9@naver.com>
2024-09-23 12:17:33 +0900

    [fix] #9 : API 주소 수정

--- data/app/main.py
+++ data/app/main.py
@@ -6,14 +6,14 @@ from app.voca.routes import router as voca_router
 
 app = FastAPI()
 
-@app.get("/reco/test")
+@app.get("/api/v1/reco/test")
 def hello():
     return {"Hello": "World"}
 
 # /reco/industries로 라우터 포함
-app.include_router(industries_router, prefix="/reco/industries")
+app.include_router(industries_router, prefix="/api/v1/reco/industries")
 
 # /reco/crawling으로 라우터 포함 (필요한 경로로 설정)
-app.include_router(crawling_router, prefix="/reco/crawling")
+app.include_router(crawling_router, prefix="/api/v1/reco/crawling")
 
-app.include_router(voca_router, prefix="/reco/voca")
+app.include_router(voca_router, prefix="/api/v1/reco/voca")
