3a36c31f338a6bca1d1e6c2437109a1db974c195
김동건 <sky4tony@naver.com>
2024-09-23 10:24:31 +0900

    [Feat] 스웨거 배포 서버 주소 변경

--- newscrab/src/main/java/com/gihojise/newscrab/config/SwaggerConfig.java
+++ newscrab/src/main/java/com/gihojise/newscrab/config/SwaggerConfig.java
@@ -23,7 +23,7 @@ import org.springframework.context.annotation.Configuration;
                 contact = @Contact(name = "gihojise", email = "your.email@example.com")
         ),
         servers = @Server(
-                url = "http://localhost:8080", // 실제 배포된 URL로 변경 필요
+                url = "https://newscrab.duckdns.org", // 실제 배포된 URL로 변경 필요
                 description = "Default Server URL"
         )
 )
