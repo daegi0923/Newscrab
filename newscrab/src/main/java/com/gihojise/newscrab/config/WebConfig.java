7f049279011da5660038529ac9c1443df292a026
차봉석 <chaqhdtjr7@gmail.com>
2024-09-23 17:48:20 +0900

    [Feat] #1 Logging interceptor 설정 Add request logging interceptor for all API endpoints

---
+++ newscrab/src/main/java/com/gihojise/newscrab/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.gihojise.newscrab.config;
+
+import com.gihojise.newscrab.interceptor.LoggingInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoggingInterceptor loggingInterceptor;
+
+    @Autowired
+    public WebConfig(LoggingInterceptor loggingInterceptor) {
+        this.loggingInterceptor = loggingInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor)
+                .addPathPatterns("/**"); // 모든 경로에 대해 인터셉터를 적용
+    }
+}
