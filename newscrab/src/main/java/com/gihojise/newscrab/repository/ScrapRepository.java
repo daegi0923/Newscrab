6f6d3762fc081faa1c1f619536ed688cd1e51e28
차봉석 <chaqhdtjr7@gmail.com>
2024-09-27 15:13:52 +0900

    [Feat] #14 스크랩 추가시 이미 스크랩한 기사인지 검사

--- newscrab/src/main/java/com/gihojise/newscrab/repository/ScrapRepository.java
+++ newscrab/src/main/java/com/gihojise/newscrab/repository/ScrapRepository.java
@@ -6,9 +6,12 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ScrapRepository extends JpaRepository<Scrap, Integer> {
     List<Scrap> findByUserUserId(int userId);
+
+    Optional<Scrap> findByUserUserIdAndNewsNewsId(int userId, int newsId);
     // 추가적인 커스텀 쿼리가 필요한 경우 메서드를 여기에 정의할 수 있습니다.
 }
