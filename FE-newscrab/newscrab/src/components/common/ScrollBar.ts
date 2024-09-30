87fcffb95db72c709dd44699c2b8332bee4cad57
박범준 <fizd7@naver.com>
2024-09-30 21:38:38 +0900

    [Style] #4 : 페이지네이션 이동시 상단이동, 스크롤바 css 글로벌적용

---
+++ FE-newscrab/newscrab/src/components/common/ScrollBar.ts
@@ -0,0 +1,40 @@
+import { css } from "styled-components";
+
+const scrollbar = css`
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #888;
+    border-radius: 12px;
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: #666;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+`;
+
+export default scrollbar;
+
+// 적용 예시: NewsContent에 적용
+// const NewsContent = styled.div`
+//   width: 60%;
+//   padding-right: 20px;
+//   border: 1px solid #ddd;
+//   border-radius: 8px;
+//   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+//   padding: 15px 100px;
+//   background-color: #fff;
+//   max-height: 680px;
+//   overflow-y: auto;
+//   position: relative;
+
+//   ${customScrollbar}; /* 스크롤바 스타일 적용 */
+//   user-select: text;
+// `;
--- FE-newscrab/newscrab/src/pages/news/detail/NewsDetailArticle.tsx
+++ FE-newscrab/newscrab/src/pages/news/detail/NewsDetailArticle.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import scrollbar from "@components/common/ScrollBar";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@store/index";
 import {
@@ -26,24 +27,7 @@ const NewsContent = styled.div`
   max-height: 680px;
   overflow-y: auto;
   position: relative;
-
-  &::-webkit-scrollbar {
-    width: 8px;
-  }
-
-  &::-webkit-scrollbar-thumb {
-    background-color: #888;
-    border-radius: 12px;
-    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
-  }
-
-  &::-webkit-scrollbar-thumb:hover {
-    background-color: #666;
-  }
-
-  &::-webkit-scrollbar-track {
-    background-color: transparent;
-  }
+  ${scrollbar}
   user-select: text;
 `;
 
