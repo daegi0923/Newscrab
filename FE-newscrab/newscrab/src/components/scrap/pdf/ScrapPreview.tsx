f433dbf964a392032c7888fad79d104f7a3db002
여대기 <daegi0923@naver.com>
2024-10-04 13:19:12 +0900

    Revert "Merge branch 'front/scrap-pdf-fix' into 'fe-develop'"
    This reverts merge request !182

--- FE-newscrab/newscrab/src/components/scrap/pdf/ScrapPreview.tsx
+++ FE-newscrab/newscrab/src/components/scrap/pdf/ScrapPreview.tsx
@@ -22,29 +22,25 @@ const ScrapPreview: React.FC<handleChangePage> = ({ funcChangePage }) => {
       const content = contentRefs.current[i];
       if (content) {
         const canvas = await html2canvas(content, {
-          scale: 1,  // 스케일을 고정
-          useCORS: true, // 크로스 도메인 이미지 처리를 위한 옵션
-          logging: true // 디버깅을 위한 로깅 활성화
+          scale: 2,  // 고정된 스케일 값 설정
         });
-  
-        // 고정된 배율로 캔버스를 다시 조정
         const imgData = canvas.toDataURL('image/png');
-  
+
         // 첫 페이지가 아니면 새로운 페이지 추가
         if (i > 0) {
           pdf.addPage();
         }
-  
+
         // PDF 페이지에 이미지 크기를 조정하여 잘리지 않도록 설정
         const imgWidth = pdf.internal.pageSize.getWidth();
         const imgHeight = (canvas.height * imgWidth) / canvas.width;
         pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
       }
     }
-  
+
     pdf.save('scrap_list.pdf');
   };
-  
+
   return (
     <>
       <div style={styles.body}>
@@ -71,13 +67,14 @@ const styles = {
   body: {
     justifyContent: 'center',
     alignItems: 'center',
-    height : '64vh',
-    overflowY: 'scroll',
+    width:'1000px',
+    paddingTop:'100px',
   },
 };
 
 const Button = styled.button`
   padding: 10px 20px;
+  margin: 10px;
   background-color: #007bff;
   color: white;
   border: none;
@@ -94,8 +91,7 @@ const Button = styled.button`
 const ModalFooter = styled.footer`
   display: flex;
   justify-content: space-between;
-  bottom : 0;
-  border-top : 1px solid #ddd;
-  padding : 15px;
-  `;
+  padding-top: 16px;
+  border-top: 1px solid #ddd;
+`;
 export default ScrapPreview;
