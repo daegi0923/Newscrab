fad09496f462d71e34d11b9f57e84e820955bccd
박범준 <fizd7@naver.com>
2024-10-05 18:34:31 +0900

    [Feat] #4 : 서치바 컴포넌트에 디바운스 적용 & 뉴스목록에 검색기능 추가

--- FE-newscrab/newscrab/src/components/common/SearchBar.tsx
+++ FE-newscrab/newscrab/src/components/common/SearchBar.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { debounce } from "lodash";
 
 const Container = styled.div`
   display: flex;
@@ -60,8 +61,28 @@ const SearchBar: React.FC<SearchBarProps> = ({
   searchText,
   onSearchChange,
 }) => {
+  const [inputValue, setInputValue] = useState(searchText);
+
+  // inputValue가 변경될 때 debounce를 직접 실행
+  useEffect(() => {
+    const debouncedChange = debounce((value: string) => {
+      onSearchChange(value);
+    }, 500);
+
+    debouncedChange(inputValue);
+
+    return () => {
+      debouncedChange.cancel(); // 언마운트 시 debounce 정리
+    };
+  }, [inputValue, onSearchChange]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
   const handleClear = () => {
-    onSearchChange(""); // Clear the input when the clear button is clicked
+    setInputValue(""); // Clear input value
+    onSearchChange(""); // 즉시 검색어 초기화
   };
 
   return (
@@ -71,10 +92,10 @@ const SearchBar: React.FC<SearchBarProps> = ({
         <SearchInput
           type="text"
           placeholder="Search..."
-          value={searchText}
-          onChange={(e) => onSearchChange(e.target.value)} // Trigger search on change
+          value={inputValue}
+          onChange={handleChange}
         />
-        <ClearButton $isVisible={searchText.length > 0} onClick={handleClear}>
+        <ClearButton $isVisible={inputValue.length > 0} onClick={handleClear}>
           ✕
         </ClearButton>
       </SearchBarContainer>
